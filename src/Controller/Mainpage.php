e70677ca175143f304ae718da5ae69f38c8a6e0e
Julien BONZOM <[joffo@example.com](mailto:bonzomj@gmail.com)>
2023-01-30 16:18:01 +0100

    first_commit

---
+++ src/Controller/Mainpage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class Mainpage extends AbstractController
+
+{
+    #[Route('/main/', name: 'main')]
+    public function index(): Response
+
+    {
+
+        return $this->render('mainpage/main.html.twig', [
+        ]);
+
+    }
+
+}
