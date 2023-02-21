b0b50df58d6cbe349a8f67e27ed2dc912270f906
Julien BONZOM <[joffo@example.com](mailto:bonzomj@gmail.com)>
2023-02-21 15:08:53 +0100

    final1

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,95 +1,78 @@
-/*let bouton = ['','','','','',''];*/
-/*let cacher = ['','','','','',''];*/
-/*for (let pas = 1; pas < {{ iteration +1 }} ; pas++) {*/
+    /*let bouton = ['','','','','',''];
+    let cacher = ['','','','','',''];
+    for (let pas = 0; pas < 6 ; pas++) {
 
-/*bouton[pas]='bouton' + pas;*/
-/*cacher[pas]='bouton' + pas */
+        bouton[pas] = 'bouton' + pas;
+        cacher[pas] = 'caché' + pas
 
-/* window [bouton[pas]]= document.getElementById(bouton[pas]);*/
-/*window [cacher[pas]] = document.getElementById(cacher[pas]);*/
-}
+        let window(boutton = document.getElementById(bouton[pas]);
+        let caché = document.getElementById(cacher[pas]);
+    }
+    for (let pas = 0; pas < 6 ; pas++) {
+        window [bouton[pas]].addEventListener('click', function () {
+            if (document.getElementById(cacher[pas]).style.display === "none") {
+                window [cacher[pas]].style.display = "block";
+                window[bouton[pas]].src = "menu2.jpg";
+                window[bouton[pas]].title = "Cliquez pour réduire";
+            }
+            else
+            {
+                window [cacher[pas]].style.display = "none";
+                window[bouton[pas]].src = "menu.jpg";
+                window[bouton[pas]].title = "Cliquez pour étendre";
+
+            }
+    });*//
+
+
+    let bouton1 = document.getElementById("bouton1");
+    let bouton2 = document.getElementById("bouton2");
+    let bouton2 = document.getElementById("bouton3");
+    let bouton3 = document.getElementById("bouton4");
+    let bouton4 = document.getElementById("bouton5");
+    let bouton5 = document.getElementById("bouton6");
+
+    let cacher1 = document.getElementById("cacher1");
+    let cacher2 = document.getElementById("cacher2");
+    let cacher3 = document.getElementById("cacher3");
+    let cacher4 = document.getElementById("cacher4");
+    let cacher5 = document.getElementById("cacher5");
+    let cacher6 = document.getElementById("cacher6");
 
 
 
-document.getElementById("bouton1").addEventListener("click", function(){
-    if (document.getElementById("cacher1").style.display === "none") {
-        document.getElementById("cacher1").style.display = "block";
-        document.getElementById("bouton1").src="./images/menu2.jpg";
-        document.getElementById("bouton1").title="Cliquez pour réduire";
-    } elseif {
-        document.getElementById("cacher1")style.display  = "none";
-        document.getElementById("bouton1").src="./images/menu.jpg";
-        document.getElementById("bouton1").title="Cliquez pour étendre";
+    cliquage()
 
-    }});
+    function cliquage() {
+        bouton1.addEventListener('click', function () {
+                if (document.getElementById(cacher1).style.display === "none") {
+                    cacher1.style.display = "block";
+                    bouton1.src = "menu2.jpg";
+                    bouton1.title = "Cliquez pour réduire";
+                } else {
+                    cacher1.style.display = "none";
+                    bouton1.src = "menu.jpg";
+                    bouton1.title = "Cliquez pour étendre";
 
-document.getElementById("bouton2").addEventListener("click", function(){
-    if (document.getElementById("cacher2").style.display === "none") {
-        document.getElementById("cacher2").style.display = "block";
-        document.getElementById("bouton2").src="./images/menu2.jpg";
-        document.getElementById("bouton2").title="Cliquez pour réduire";
-    } elseif {
-        document.getElementById("cacher2")style.display  = "none";
-        document.getElementById("bouton2").src="./images/menu.jpg";
-        document.getElementById("bouton2").title="Cliquez pour étendre";
-
-    }});
-document.getElementById("bouton3").addEventListener("click", function(){
-    if (document.getElementById("cacher3").style.display === "none") {
-        document.getElementById("cacher3").style.display = "block";
-        document.getElementById("bouton3").src="./images/menu2.jpg";
-        document.getElementById("bouton3").title="Cliquez pour réduire";
-    } elseif {
-        document.getElementById("cacher3").style.display  = "none";
-        document.getElementById("bouton3").src="./images/menu.jpg";
-        document.getElementById("bouton3").title="Cliquez pour étendre";
-
-    }});
-document.getElementById("bouton4").addEventListener("click", function(){
-    if (document.getElementById("cacher4").style.display === "none") {
-        document.getElementById("cacher4").style.display = "block";
-        document.getElementById("bouton4").src="./images/menu2.jpg";
-        document.getElementById("bouton4").title="Cliquez pour réduire";
-    } elseif {
-        document.getElementById("cacher4")style.display  = "none";
-        document.getElementById("bouton4").src="./images/menu.jpg";
-        document.getElementById("bouton4").title="Cliquez pour étendre";
-
-    }});
-document.getElementById("bouton5").addEventListener("click", function(){
-    if (document.getElementById("cacher5").style.display === "none") {
-        document.getElementById("cacher5").style.display = "block";
-        document.getElementById("bouton5").src="./images/menu2.jpg";
-        document.getElementById("bouton5").title="Cliquez pour réduire";
-    } elseif {
-        document.getElementById("cacher5")style.display  = "none";
-        document.getElementById("bouton5").src="./images/menu.jpg";
-        document.getElementById("bouton5").title="Cliquez pour étendre";
-
-    }});
+                }
+            }
+        )}
 
 
 
 
 
-
-
-
-
-
-
-
-function AdapterDivAResolution() {
+    function AdapterDivAResolution() {
     let x_res = screen.width;
     let y_res = screen.height;
     const bouton1 = document.getElementById("bouton1")
     if(x_res < 1024)
-    {
-        bouton1.src="./images/menu2.jpg";
-        bouton1.style.display = "block";
-        bouton1.style.visibility="visible";
-    }
+{
+    bouton1.src="./images/menu2.jpg";
+    bouton1.style.display = "block";
+    bouton1.style.visibility="visible";
 }
-AdapterDivAResolution()
-bouton1.addEventListener('click',cliquage, false);
+}
+    AdapterDivAResolution()
+    bouton1.addEventListener('click',cliquage, false);
 
